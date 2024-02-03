1563661fe9372c1029b02e0675c884459876f466
Marcos Aurelio Wozhiak Junior de Oliveira <aureliowozhiak@gmail.com>
2024-02-03 13:07:16 -0300

    some updates

--- app/main.py
+++ app/main.py
@@ -4,15 +4,17 @@ from datetime import datetime
 import pandas as pd
 import json
 import time
+import os
 
 payload_generator = PayloadGenerator()
 
 for i in range(0):
     payload = payload_generator.generate_payload()
     current_time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-    with open(f"/app/events/payload_{current_time}.json", "w") as file:
+    with open(f"/app/events/json/payload_{current_time}.json", "w") as file:
         file.write(str(json.dumps(payload, indent=4)))
     time.sleep(1)
 
 pipeline = Pipeline("dev")
 pipeline.get_events()
+
--- app/pipeline.py
+++ app/pipeline.py
@@ -1,29 +1,35 @@
-from pyspark.sql import SparkSession
 import os
-from pyspark.sql import SparkSession
+import json
+import pandas as pd
+import time
+
 
 
 class Pipeline:
     def __init__(self, environment = "dev"):
         self.environment = environment
+        os.makedirs(f"/app/events/parquet/", exist_ok=True)
 
 
     def get_events(self):
         match self.environment:
             case "dev":
-                spark = SparkSession.builder \
-                    .appName("JSON to Parquet Conversion") \
-                    .getOrCreate()
+                
+                            
+                for file in os.listdir("/app/events/json/"):
+                    if file.endswith(".json"):
+                        with open(f"/app/events/json/{file}", "r") as json_file:
+                            json_data = json_file.read()
+                        
 
-                json_path = "/app/events/payload_2024_02_02_12_38_11.json"
+                        df = pd.json_normalize(json.loads(json_data))
+                        domain = df["domain"].values[0]
+                        event_type = df["event_type"].values[0]
+                                        
+                        os.makedirs(f"/app/events/parquet/{domain}", exist_ok=True)
+                        os.makedirs(f"/app/events/parquet/{domain}/{event_type}", exist_ok=True)
 
-                df = spark.read.json(json_path)
-
-                parquet_path = "/app/events/event.parquet"
-                if not os.path.exists(parquet_path):
-                    df.write.parquet(parquet_path)
-                else:
-                    raise FileExistsError("File already exists")
+                        df.to_parquet(f"/app/events/parquet/{domain}/{event_type}/{file.split(".")[0]}.parquet")
 
             case _:
                 raise ValueError("Invalid environment")                
